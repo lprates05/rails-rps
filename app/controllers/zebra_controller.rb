01fdd076c761dfd029fa776b9a7f52a6bd57b072
lprates05 <lorenapratesdl@outlook.com>
2025-04-21 14:51:50 +0000

    rps

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,12 @@
+
+class ZebraController < ApplicationController
+
+  def giraffe
+
+    @random_move = ["rock", "paper", "scissors"].sample
+
+   
+  
+    render ({ :template => "game_templates/play_rock" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/rock", { :controller => "zebra", :action => "giraffe" })
+  
 end
